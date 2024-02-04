6fbae2ce8750b143228741abd9dd084758c0c8b6
eugene <113156209+eugene91cn@users.noreply.github.com>
2024-02-04 10:08:43 +0800

    预约前随机延时若干秒
    预约前随机延时若干秒

--- campus-modular/src/main/java/com/oddfar/campus/business/service/impl/IMTServiceImpl.java
+++ campus-modular/src/main/java/com/oddfar/campus/business/service/impl/IMTServiceImpl.java
@@ -555,10 +555,14 @@ public class IMTServiceImpl implements IMTService {
     }
 
     public JSONObject reservation(IUser iUser, String itemId, String shopId) {
+
         
+        // 生成随机秒数 
+        int randomSecond = random.nextInt(60);
+        Thread.sleep(randomSecond);
         // 生成随机毫秒数 
-        int randomMillisecond = random.nextInt(60000);
-        Thread.sleep(randomMillisecond);
+        int randomMillisecond = random.nextInt(1000);        
+        Thread.sleep(randomSecond*1000+randomMillisecond);
         
         Map<String, Object> map = new HashMap<>();
         JSONArray itemArray = new JSONArray();
